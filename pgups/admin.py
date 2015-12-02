4df64ae07e7b97adf718fef7f5e224818a61ba76
judequinn <judequinn1337@gmail.com>
2015-12-02 17:38:56 +0300

    judequinn.1 created forms, views and template to display registration form

--- pgups/admin.py
+++ pgups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import  Competition, Team, Person, Age, Style, Distance, Request, Tour, Competitor, Start, Order, Result
+from .models import  Competition, Team, Person, Age, Style, Distance, Userrequest, Tour, Competitor, Start, Order, Result
 
 admin.site.register(Competition)
 admin.site.register(Team)
@@ -7,7 +7,7 @@ admin.site.register(Person)
 admin.site.register(Age)
 admin.site.register(Style)
 admin.site.register(Distance)
-admin.site.register(Request)
+admin.site.register(Userrequest)
 admin.site.register(Tour)
 admin.site.register(Competitor)
 admin.site.register(Start)
--- pgups/views.py
+++ pgups/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .forms import RequestForm
+from .forms import PersonForm
 
-# Create your views here.
+
+def get_client_ip(request):				# Получает IP пользователя
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+
+def reg_request(request):                # Сохраняет данные из формы в БД
+	
+	if request.method == "POST":
+		request_form = RequestForm(request.POST)
+		person_form = PersonForm(request.POST)
+		if person_form.is_valid() and request_form.is_valid():
+			person = person_form.save()
+			person.save()
+			userrequest = request_form.save(commit=False)
+			userrequest.ip = get_client_ip(request)
+			userrequest.save()
+			request_form = RequestForm() 
+			person_form = PersonForm()
+			return render(request, 'pgups/reg.html', {'request_form' : request_form, 'person_form' : person_form}) 
+	else:
+		request_form = RequestForm()
+		person_form = PersonForm()
+		return render(request, 'pgups/reg.html', {'request_form' : request_form, 'person_form' : person_form})
