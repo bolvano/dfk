0f224773ed9495ddba6b0ba7b68e95ed7b66a5bb
judequinn <judequinn1337@gmail.com>
2015-12-03 16:45:22 +0300

    judequinn.1 competitors are now saved automatically after submitting the form, added empty labels to choice fields, added success message display

--- pgups/forms.py
+++ pgups/forms.py
@@ -1,22 +1,23 @@
 from django import forms
-from .models import Userrequest
-from .models import Person
+from .models import Userrequest, Person, Competition, Team, Competitor, Tour
 
 
 class RequestForm(forms.ModelForm):
+
+    competition = forms.ModelChoiceField(Competition.objects.all(), empty_label='Выберите соревнование из списка:', label='Соревнования:')
+    team = forms.ModelChoiceField(Team.objects.all(), empty_label='Выберите команду:', label='Спортивное общество (команда):')
 
     class Meta:
 
         model = Userrequest
         exclude = ['ip', 'date']
         labels = {
-            'competition': ('Соревнования:'),
-            'team': ('Спортивное общество (команда):'),
-            'representative': ('Представитель команды:'),
+
+            'representative': ('Представитель команды (ФИО):'),
             'phone': ('Телефон:'),
             'email': ('Электронная почта:'),
         }
-
+        
 
 class PersonForm(forms.ModelForm):
 
@@ -29,4 +30,20 @@ class PersonForm(forms.ModelForm):
             'last_name': ('Фамилия:'),
             'birth_year': ('Год рождения:'),
             'gender': ('Пол:'),
+        }
+
+class CompetitorForm(forms.ModelForm):
+
+    tour = forms.ModelChoiceField(Tour.objects.all(), empty_label='Выберите тур:', label='Тур:')
+    prior_time = forms.FloatField(required=False, label='Предварительное время:', widget=forms.TextInput())
+
+    class Meta:
+
+        model = Competitor
+        exclude = ['approved', 'person', 'userrequest', 'age']
+        labels = {
+
+            'prior_time' : ('Предварительное время:'),
+            'main_distance' : ('Основная дистанция:')
+
         }
