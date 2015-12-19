78c7d76080e8c568a6e0d13c12d0fc36cc2c7820
Nik <nik.panov.mobile@gmail.com>
2015-12-20 01:37:27 +0300

    finished form, started tours

--- pgups/urls.py
+++ pgups/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.reg_request, name='regrequest'),
-    url(r'^r$', views.reg_request2, name='regrequest2'),
+    url(r'^tours/$', views.tours),
+    url(r'^tour/(?P<tour_id>\d+)/$', views.tour_starts),
+    url(r'^get_tours/(?P<age>\d+)/$', views.get_tours),
+    #(r'^user/(?P<username>\w{0,50})/$', views.profile_page,),
+
     #url(r'^person_form$', views.person_form) # ajax
 ]
