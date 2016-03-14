5b5984277e01d9bdaa4dcba974c43cbe245a882a
judequinn <fromabbeyroad@yandex.ru>
2016-03-14 20:53:53 +0300

    judequinn.angularjs created validationApp and new person formset, added cdn links for bootstrap and jQuery, css refactoring

--- pgups/models.py
+++ pgups/models.py
@@ -2,7 +2,6 @@
 
 from django.db import models
 
-# Create your models here.
 
 #Соревнования
 class Competition(models.Model):
--- pgups/static/js/validation-app.js
+++ pgups/static/js/validation-app.js
@@ -1,1 +1,26 @@
 var validationApp = angular.module('validationApp', []);
+
+
+// custom config handling conflicting django/angular template tags
+validationApp.config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+});
+
+// form controller
+validationApp.controller('formCtrl', ['$scope', function($scope, $http) {
+
+    $scope.form = {};
+
+    $scope.persons = [{ id: 'person-0'}, { id: 'person-1'}];
+
+    $scope.addPerson = function() {
+        var newPersonNum = $scope.persons.length;
+        $scope.persons.push({'id':'person-'+newPersonNum});
+    };
+
+    $scope.removePerson = function(idx) {
+        $scope.persons.splice(idx, 1);
+    };
+
+}]);
