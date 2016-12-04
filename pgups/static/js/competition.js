0f6d7d605995b58c72c02ede1d9fd57bab54d234
Jude Quinn <fromabbeyroad@yandex.ru>
2016-12-04 10:26:14 +0300

    Переделала страницу competition.html, добавила angular-ui-bootstrap

---
+++ pgups/static/js/competition.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    angular
+    .module('competitionApp', ['ui.bootstrap'])
+    .controller('ModalInstanceController', ModalInstanceController)
+    .controller('CompetitionController', CompetitionController)
+    .config(altTemplateTags);
+
+    function altTemplateTags($interpolateProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+    }
+
+    function CompetitionController($scope, $uibModal, $log) {
+
+        var vm = this;
+        vm.openModal = openModal;
+
+        function openModal() {
+            var modalInstance = $uibModal.open({
+              animation: true,
+              templateUrl: 'modal.html',
+              controller: 'ModalInstanceController',
+              controllerAs: '$ctrl',
+              size: 'md'
+            });
+        }
+    }
+
+    function ModalInstanceController($uibModalInstance) {
+        var $ctrl = this;
+
+        $ctrl.cancel = function () {
+          $uibModalInstance.dismiss('cancel');
+        };
+    }
+})();
